e3c894a94c0add491ee0e29e49a83d12938bdd0d
Nelson Oliveira <nholiveira@ciasc.sc.gov.br>
2022-06-19 14:14:49 -0300

    1 - mysql - user insert + signup

--- articles/app.py
+++ articles/app.py
@@ -1,20 +1,29 @@
-from flask import Flask, render_template, flash, request
+from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField
 from wtforms.validators import DataRequired
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
 import logging
 
 logger = logging.getLogger("werkzeug")
 db = SQLAlchemy()
 
 
+# Formularios
 class LoginForm(FlaskForm):
     username = StringField("username", validators=[DataRequired()])
     password = PasswordField("password", validators=[DataRequired()])
 
 
+class SignupForm(FlaskForm):
+    username = StringField("username", validators=[DataRequired()])
+    email = StringField("email", validators=[DataRequired()])
+    password = PasswordField("password", validators=[DataRequired()])
+
+
+# Databases
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
@@ -40,6 +49,7 @@ def create_app():
     def index():
         return "ARTICLES"
 
+    # Rota para LOGIN
     @app.route("/login", methods=['GET', 'POST'])
     def login():
         if request.method == 'GET':
@@ -48,7 +58,7 @@ def create_app():
         else:
             username = request.form['username']
             password = request.form['password']
-            
+
             user = User.query.filter_by(username=username).first()
 
             if user:
@@ -56,12 +66,49 @@ def create_app():
 
             else:
                 print("novo usuário")
-            
-            print(username)
-            print(password)
+
+            print('username.....: ' + str(username))
+            print('password.....: ' + str(password))
             form = LoginForm()
             form.username.data = username
             form.password.data = password
-            return render_template("login.html", FORM=form)
+            return redirect(url_for('signup', FORM=form))
+
+    # Rota para SIGNUP
+    @app.route("/signup", methods=['GET', 'POST'])
+    def signup():
+        if request.method == 'GET':
+            form = SignupForm()
+            return render_template("signup.html", FORM=form)
+        else:
+            print(request.form)
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+
+            user_exists = User.query.filter_by(username=username).first()
+            email_exists = User.query.filter_by(email=email).first()
+
+            if user_exists or email_exists:
+                print("user já existe")
+
+            else:
+                print("novo usuário")
+                new_user = User(
+                    username=username,
+                    email=email,
+                    password=generate_password_hash(password, method='md5'))
+                #print(dir(db.session))
+                db.session.add(new_user)
+                db.session.commit()
+
+            print('username.....: ' + str(username))
+            print('email........: ' + str(email))
+            print('password.....: ' + str(password))
+            form = SignupForm()
+            form.username.data = username
+            form.email.data = email
+            form.password.data = password
+            return render_template('signup.html', FORM=form)
 
     return app
