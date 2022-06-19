f0de7b21e9f1c959b67d53a8fa4354c1d8681617
Nelson Oliveira <nholiveira@ciasc.sc.gov.br>
2022-06-19 11:21:44 -0300

    1 - sqlalchemy + mysql

--- articles/app.py
+++ articles/app.py
@@ -3,9 +3,11 @@ from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField
 from wtforms.validators import DataRequired
 from flask_login import LoginManager
+from flask_sqlalchemy import SQLAlchemy
 import logging
 
 logger = logging.getLogger("werkzeug")
+db = SQLAlchemy()
 
 
 class LoginForm(FlaskForm):
@@ -13,14 +15,26 @@ class LoginForm(FlaskForm):
     password = PasswordField("password", validators=[DataRequired()])
 
 
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(100), unique=True)
+    email = db.Column(db.String(100), unique=True)
+    password = db.Column(db.String(100))
+
+
 def create_app():
+
     app = Flask(__name__)
+
     app.config["SECRET_KEY"] = "secret"
+    app.config["SQLALCHEMY_DATABASE_URI"] = 'mysql+mysqlconnector://nhoj:123456@localhost/articles'
     app.config["DEBUG"] = True
 
-    login_manager = LoginManager()
-    login_manager.login_view = 'auth.login'
-    login_manager.init_app(app)
+    db.init_app(app)
+
+    #login_manager = LoginManager()
+    #login_manager.login_view = 'auth.login'
+    #login_manager.init_app(app)
 
     @app.route("/")
     def index():
@@ -34,6 +48,15 @@ def create_app():
         else:
             username = request.form['username']
             password = request.form['password']
+            
+            user = User.query.filter_by(username=username).first()
+
+            if user:
+                print("user já existe")
+
+            else:
+                print("novo usuário")
+            
             print(username)
             print(password)
             form = LoginForm()
