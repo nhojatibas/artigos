f5e9766ea5f6943a95be7f82583efdea534f45b7
Nelson Oliveira <nholiveira@ciasc.sc.gov.br>
2022-06-21 19:09:04 -0300

    3 - login + profile + index

--- articles/app.py
+++ articles/app.py
@@ -2,7 +2,7 @@ from flask import Flask, render_template, flash, request, redirect, url_for
 from flask_wtf import FlaskForm
 from wtforms import BooleanField, PasswordField, StringField
 from wtforms.validators import DataRequired
-from flask_login import LoginManager
+from flask_login import LoginManager, UserMixin, login_user, logout_user
 from flask_sqlalchemy import SQLAlchemy
 from werkzeug.security import generate_password_hash, check_password_hash
 import logging
@@ -24,7 +24,7 @@ class SignupForm(FlaskForm):
 
 
 # Databases
-class User(db.Model):
+class User(UserMixin, db.Model):
     id = db.Column(db.Integer, primary_key=True)
     username = db.Column(db.String(100), unique=True)
     email = db.Column(db.String(100), unique=True)
@@ -41,38 +41,50 @@ def create_app():
 
     db.init_app(app)
 
-    #login_manager = LoginManager()
-    #login_manager.login_view = 'auth.login'
-    #login_manager.init_app(app)
+    login_manager = LoginManager()
+    login_manager.login_view = 'login'
+    login_manager.init_app(app)
+    print("ĺogin_manager_______")
+
+    @login_manager.user_loader
+    def load_user(user_id):
+        print(user_id)
+        return User.query.get(int(user_id))
 
     @app.route("/")
     def index():
-        return "ARTICLES"
+        return render_template("index.html")
 
-    # Rota para LOGIN
-    @app.route("/login", methods=['GET', 'POST'])
-    def login():
-        if request.method == 'GET':
-            form = LoginForm()
-            return render_template("login.html", FORM=form)
+    @app.route("/login", methods=['GET'])
+    def login_get():
+        form = LoginForm()
+        return render_template("login.html", FORM=form)
+
+    @app.route("/login", methods=['POST'])
+    def login_post():
+        loginform = LoginForm()
+        signupform = SignupForm()
+        username = request.form['username']
+        password = request.form['password']
+
+        user = User.query.filter_by(username=username).first()
+
+        if not user:
+            flash("Por favor, faça cadastramento para acesso")
+            return redirect(url_for('signup', FORM=signupform))
+        elif not check_password_hash(user.password, password):
+            flash('Senha incorreta')
+            return redirect(url_for('login_post', FORM=loginform))
         else:
-            username = request.form['username']
-            password = request.form['password']
+            login_user(user)
+            flash('Bem Vindo')
+            return redirect(url_for('profile', USERID=user.id))
 
-            user = User.query.filter_by(username=username).first()
-
-            if user:
-                print("user já existe")
-
-            else:
-                print("novo usuário")
-
-            print('username.....: ' + str(username))
-            print('password.....: ' + str(password))
-            form = LoginForm()
-            form.username.data = username
-            form.password.data = password
-            return redirect(url_for('signup', FORM=form))
+    # Rota para LOGOUT
+    @app.route("/logout")
+    def logout():
+        logout_user()
+        return redirect(url_for('login_get'))
 
     # Rota para SIGNUP
     @app.route("/signup", methods=['GET', 'POST'])
@@ -81,34 +93,31 @@ def create_app():
             form = SignupForm()
             return render_template("signup.html", FORM=form)
         else:
-            print(request.form)
-            username = request.form['username']
-            email = request.form['email']
-            password = request.form['password']
+            form = SignupForm()
+            form.username.username = request.form['username']
+            form.username.email = request.form['email']
+            form.username.password = request.form['password']
 
-            user_exists = User.query.filter_by(username=username).first()
-            email_exists = User.query.filter_by(email=email).first()
+            user_exists = User.query.filter_by(username=form.username.username).first()
+            email_exists = User.query.filter_by(email=form.username.email).first()
 
             if user_exists or email_exists:
-                print("user já existe")
-
+                flash('user já existe')
+                return redirect(url_for('signup', FORM=form))
             else:
-                print("novo usuário")
+                print("cadastrando novo usuário")
                 new_user = User(
-                    username=username,
-                    email=email,
-                    password=generate_password_hash(password, method='md5'))
-                #print(dir(db.session))
+                    username=form.username.username,
+                    email=form.username.email,
+                    password=generate_password_hash(form.username.password, method='md5'))
                 db.session.add(new_user)
                 db.session.commit()
+            return render_template('login.html', FORM=form)
 
-            print('username.....: ' + str(username))
-            print('email........: ' + str(email))
-            print('password.....: ' + str(password))
-            form = SignupForm()
-            form.username.data = username
-            form.email.data = email
-            form.password.data = password
-            return render_template('signup.html', FORM=form)
+    # Rota para PROFILE
+    @app.route("/profile", methods=['GET', 'POST'])
+    def profile():
+        user = load_user(request.args['USERID'])
+        return render_template('profile.html', NOME=user.username)
 
     return app
